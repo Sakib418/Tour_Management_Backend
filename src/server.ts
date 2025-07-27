bd34637bca8f00f6af68d7793b69cac55edfbf2a
Sakib418 <ifthekerskb@gmail.com>
2025-07-28 00:32:06 +0600

    project setup done

---
+++ src/server.ts
@@ -0,0 +1,75 @@
+
+import {Server} from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./app/config/env";
+
+let server : Server;
+
+
+
+const startServer = async () => {
+    try {
+         await mongoose.connect(envVars.DB_URL)
+
+console.log("Connected to DB");
+
+server = app.listen(envVars.PORT, () => {
+    console.log(`Server is listening to post ${envVars.PORT}`)
+})
+    } catch (error) {
+        console.log(error);
+    }
+   
+}
+
+startServer()
+
+process.on("SIGINT", (err) => {
+    console.log("SIGINT signal recieved... Server is shutting down..", err);
+
+    if(server){
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+     process.exit(1)
+})
+
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandled Rejecttion detected... Server is shutting down..", err);
+
+    if(server){
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+    process.exit(1)
+})
+
+process.on("uncoughtExeption", (err) => {
+    console.log("UncoughtExeption Rejecttion detected... Server is shutting down..", err);
+
+    if(server){
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+    process.exit(1)
+})
+
+//Unhandle exeption error
+//Promise.reject(new Error("I forgot to catch the promise"))
+
+
+process.on("SIGTERM", (err) => {
+    console.log("SIGTERM signal recieved... Server is shutting down..", err);
+
+    if(server){
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+     process.exit(1)
+})
+
